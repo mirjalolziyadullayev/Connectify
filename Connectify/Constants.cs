c0534c20c19df67cc48c2e184be7c72e1cf2ccd8
Your Name <121923945+mirjalolziyadullayev@users.noreply.github.com>
2024-02-12 11:40:08 +0500

    code

--- Connectify/Constants.cs
+++ Connectify/Constants.cs
@@ -4,4 +4,5 @@ public class Constants
     public const string USERS_PATH = @"\..\..\..\..\Connectify\Data\users.json";
     public const string CHATS_PATH = @"\..\..\..\..\Connectify\Data\chats.json";
     public const string MESSAGES_PATH = @"\..\..\..\..\Connectify\Data\messages.json";
+    public const string POSTS_PATH = @"\..\..\..\..\Connectify\Data\posts.json";
 }
--- Connectify/Interfaces/IPostService.cs
+++ Connectify/Interfaces/IPostService.cs
@@ -1,6 +1,14 @@
-﻿namespace Connectify.Interfaces;
+﻿using Connectify.Models;
 
-public class IPostService
+namespace Connectify.Interfaces;
+
+public interface IPostService
 {
-
+    ValueTask<Post> Create(Post post);
+    ValueTask<bool> Delete(int id);
+    ValueTask<Post> Get(int id);
+    ValueTask<List<Post>> GetAll();
+    ValueTask<List<Post>> GetAllByAuthorId(int id);
+    ValueTask<Post> Update(int id, int userId, Post post);
+    ValueTask<bool> IncViewsCount(int id);
 }
