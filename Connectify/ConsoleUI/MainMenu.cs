3331a59e05f9dfd4de13dc2acacda27e8944b0ba
Your Name <121923945+mirjalolziyadullayev@users.noreply.github.com>
2024-02-12 12:32:08 +0500

    littlebit

---
+++ Connectify/ConsoleUI/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using Connectify.ConsoleUI.SubMenus;
+using Connectify.Services;
+using Spectre.Console;
+
+namespace Connectify.ConsoleUI;
+
+public class MainMenu
+{
+    private UserService userService;
+    private PostService postService;
+
+    private LoginMenu loginMenu;
+    public MainMenu()
+    {
+        this.userService = new UserService();
+        this.postService = new PostService(userService);
+
+        this.loginMenu = new LoginMenu(userService);
+    }
+    public async Task Display()
+    {
+        while (true)
+        {
+            var choise = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[green]Connect[/][grey]ify[/]")
+                    .PageSize(7)
+                    .AddChoices(new[] {
+                        "Register",
+                        "Login\n",
+                        "[red]Exit[/]"}));
+            switch (choise)
+            {
+                case "Register":
+                    Console.Clear();
+                    
+                    break;
+                case "Login":
+                    Console.Clear();
+                    loginMenu.Login();
+                    break;
+                case "[red]Exit[/]":
+                    Console.Clear();
+                    return;
+            }
+        }
+    }
+}
---
+++ Connectify/ConsoleUI/SubMenus/LoginMenu.cs
@@ -0,0 +1,36 @@
+﻿using Connectify.Interfaces;
+using Connectify.Services;
+using Spectre.Console;
+
+namespace Connectify.ConsoleUI.SubMenus;
+
+public class LoginMenu
+{
+    UserService userService;
+    public LoginMenu(UserService userService)
+    {
+        this.userService = userService;
+    }
+    public async Task Login()
+    {
+        Console.Clear();
+
+        var firstname = AnsiConsole.Ask<string>("Enter your [green]FirtsName[/]:");
+        var lastname = AnsiConsole.Ask<string>("Enter your [green]LastName[/]:");
+
+        var user
+
+        var table = new Table();
+
+        table.AddColumn("Created Music");
+
+        table.AddRow($"[green]MusicID[/]: {newMusic.Id}");
+        table.AddRow($"[green]Music's Name[/]: {newMusic.Name}");
+        table.AddRow($"[green]Music's Author[/]: {newMusic.Author}");
+        table.AddRow($"[green]Music's Genre[/]: {newMusic.Genre}");
+
+        AnsiConsole.Write(table);
+
+        break;
+    }
+}
